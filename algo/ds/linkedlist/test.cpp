b05725c86859204691aad3380425d9d09f9add58
Mike Krinkin <krinkin.m.u@gmail.com>
2015-01-04 23:42:41 +0300

    update tests

--- algo/ds/linkedlist/test.cpp
+++ algo/ds/linkedlist/test.cpp
@@ -1,33 +1,96 @@
-#include <iostream>
-#include <cstdlib>
+#include <algorithm>
 #include <cassert>
+#include <cstdlib>
+#include <iostream>
+#include <iterator>
 #include <vector>
 #include "linkedlist.hpp"
 
-void run_test(size_t size)
+template <typename Ct>
+void fill_random(Ct &ct, size_t size)
+{ generate_n(std::back_inserter(ct), size, &rand); }
+
+template <typename Ct>
+void print_container(Ct const &ct)
 {
-	std::vector<int> control;
-	LinkedList<int> ints;
+	using std::begin;
+	using std::end;
 
-	for (size_t i = 0; i != size; ++i) {
-		int const number = rand();
-		control.push_back(number);
-		ints.push_back(number);
-	}
-
-	assert(ints.size() == control.size());
-	assert(std::equal(ints.begin(), ints.end(), control.begin()));
+	std::cout << "[";
+	std::for_each(begin(ct), end(ct),
+		[](typename Ct::const_reference v)
+		{ std::cout << v << " "; });
+	std::cout << "]";
 }
 
-int main(int argc, char **argv)
-{
-	if (argc < 2) {
-		std::cout << "List of integerst expected" << std::endl;
-		return 0;
-	}
+#ifdef DEBUG
+#define LOG_CONTAINER(ct)			\
+	do {					\
+		std::cout << #ct << ": ";	\
+		print_container(ct);		\
+		std::cout << std::endl;		\
+	} while (0);
+#else
+#define LOG_CONTAINER(ct) do { } while(0);
+#endif
 
-	for (int arg = 1; arg != argc; ++arg)
-		run_test(atol(*(argv + arg)));
+template <typename Ct>
+void log_container(Ct const &ct, char const *name)
+{
+	std::cout << name;
+	print_container(ct);
+	std::cout << std::endl;	
+}
+
+void run_fill_test(size_t size)
+{
+	using std::begin;
+	using std::end;
+
+	std::vector<int> source;
+	fill_random(source, size);
+	LOG_CONTAINER(source);
+
+	LinkedList<int> ints;
+	std::copy(begin(source), end(source), std::back_inserter(ints));
+	LOG_CONTAINER(ints);
+
+	assert(ints.size() == source.size());
+	assert(std::equal(begin(ints), end(ints), begin(source)));
+}
+
+void run_range_construct_test(size_t size)
+{
+	std::vector<int> source;
+	fill_random(source, size);
+	LOG_CONTAINER(source);
+
+	LinkedList<int> ints(begin(source), end(source));
+	LOG_CONTAINER(ints);
+
+	assert(source.size() == ints.size());
+	assert(std::equal(begin(source), end(source), begin(ints)));
+}
+
+void run_copy_test(size_t size)
+{
+	LinkedList<int> source;
+	fill_random(source, size);
+	LOG_CONTAINER(source);
+
+	LinkedList<int> copy(source);
+	LOG_CONTAINER(copy);
+	assert(source.size() == copy.size());
+	assert(std::equal(begin(source), end(source), begin(copy)));
+}
+
+int main(void)
+{
+	for (size_t size : {0, 10, 100, 1000, 10000}) {
+		run_fill_test(size);
+		run_range_construct_test(size);
+		run_copy_test(size);
+	}
 
 	return 0;
 }
